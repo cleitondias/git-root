86f37093afe8b728f82d90f8acf5fc7bd2a0ffe5
Cleiton <cleitondias2005@gmail.com>
2021-10-17 22:32:18 +0100

    commit on 'master'

---
+++ petshopoverview/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/ovp/app/Component"], function(AppComponent) {
+    return AppComponent.extend("cgutech.petshopoverview.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
